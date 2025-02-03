3aa10ec792a03ef8083582e0681d92eb9b23a5b9
Muhammed Sururat Omotoyosi <sururatomotoyosi@gmail.com>
2025-02-03 11:49:43 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+const p = document.querySelector('p')
+
+p.innerHTML = 'welcome to our website'
+p.style.backgroundColor = 'red'
+p.style.color = 'wheat'
+
+const button = document.querySelector('button')
+
+button.innerHTML = 'sign up here'
+button.style.backgroundColor = 'pink'
+button.style.color = 'orange'
+
+
+//creating of element
+const body = document.querySelector('body')
+
+//const h1 = document.createElement('h1')
+//h1.textContent = "How are you?"
+
+//body.append(h1)
+
+body.innerHTML = "<h1>How are you?</h1>"  
+
